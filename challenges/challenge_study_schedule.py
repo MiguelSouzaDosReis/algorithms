61cc80dbdb67942f819dd29163a0aaf77e5984c0
Miguel_Reis <guelsouzar@gmail.com>
2022-10-12 01:55:52 -0300

    req 01 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    count = 0
+    for firstNumber, secondNumber in permanence_period:
+        if (
+            target_time is None
+            or not isinstance(firstNumber, (int))
+            or not isinstance(secondNumber, (int))
+        ):
+            return None
+        if target_time == firstNumber or target_time == secondNumber:
+            count += 1
+        if target_time > firstNumber and target_time < secondNumber:
+            count += 1
+    return count
