c6e626a5c77409d7743fc04566e973a7618f26f6
Miguel_Reis <guelsouzar@gmail.com>
2022-10-12 02:04:45 -0300

    req 03 done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,54 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if first_string == "" or second_string == "":
+        return False
+    first = list(first_string.lower())
+    second = list(second_string.lower())
+    merge_sort(first)
+    merge_sort(second)
+    if first == second:
+        return True
+    else:
+        return False
+
+# Utilizei o codigo merge_sort do course
+
+
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end)
+
+
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid]
+    right = numbers[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(
+        start, end
+    ):
+        if left_index >= len(
+            left
+        ):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(
+            right
+        ):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif (
+            left[left_index] < right[right_index]
+        ):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            numbers[general_index] = right[
+                right_index
+            ]
+            right_index = right_index + 1
